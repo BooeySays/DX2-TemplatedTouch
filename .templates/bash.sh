70acf7934eb063f65978237b519d276c06e39be8
booeysays <dbuihien@my.bcit.ca>
2018-09-12 03:33:47 +0000

    folders for all the templates

---
+++ .templates/bash.sh
@@ -0,0 +1,12 @@
+##   LANG     : bash / sh
+##   CALL     : From terminal
+##   DESC	  : 
+##				
+##				
+##   CODR	  : BooeySays
+##   HTTP	  : 66MHz.ca
+##   MAIL	  : Booey@66MHz.ca
+##	 REPO	  : github.com/BooeySays
+##
+############# Copyright 66MHz 2018 #############
+
